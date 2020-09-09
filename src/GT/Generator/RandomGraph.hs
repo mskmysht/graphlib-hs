715a9cadb90d436be9c54ab0fb8e89420b69dbe7
Masaaki Miyashita <m.mysht118@gmail.com>
2020-09-09 09:02:41 +0000

    Fix format

--- src/GT/Generator/RandomGraph.hs
+++ src/GT/Generator/RandomGraph.hs
@@ -1,53 +1,70 @@
-{-# LANGUAGE Strict #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE Strict #-}
 
-module GT.Generator.RandomGraph 
-  (barabasiAlbert
+module GT.Generator.RandomGraph
+  ( barabasiAlbert,
   )
 where
 
-import GT.Graph
-import System.Random.SFMT
+import Control.Monad.Except (ExceptT (..))
 import Control.Monad.Primitive (PrimMonad)
-import Data.Set as St
-import Data.Sequence as S
+import Control.Monad.Trans (lift)
 import Data.Foldable as F (toList)
 import Data.Maybe (fromJust)
-import Control.Monad.Except (ExceptT(..))
-import Control.Monad.Trans (lift)
+import Data.Sequence as S
+  ( Seq,
+    empty,
+    fromList,
+    length,
+    replicate,
+    zip,
+    (!?),
+    (><),
+  )
+import Data.Set as St (Set, empty, insert, member, toList)
+import GT.Graph (Builder (build), EdgeId, Graph, NodeId)
+import System.Random.SFMT
+  ( MonadGen,
+    Variate (uniformR),
+    initializeFromSeed,
+  )
 
-barabasiAlbert :: forall d m n' e' g n e . (PrimMonad m, Builder g n e, Graph g n n' e e' d) =>
-  Int -- the number of node
-   -> Int -- the number of each degree
-   -> Int -- seed value
-   -> (NodeId -> n) -- node builder
-   -> (NodeId -> NodeId -> e) -- edge builder
-   -> ExceptT String m g
+barabasiAlbert ::
+  forall d m n' e' g n e.
+  (PrimMonad m, Builder g n e, Graph g n n' e e' d) =>
+  Int -> -- the number of node
+  Int -> -- the number of each degree
+  Int -> -- seed value
+  (NodeId -> n) -> -- node builder
+  (NodeId -> NodeId -> e) -> -- edge builder
+  ExceptT String m g
 barabasiAlbert n m seed nb eb
   | m < 1 || m >= n = ExceptT $ return $ Left $ "Barabási–Albert network must have m >= 1 and m < n, m = " ++ show m ++ ", n = " ++ show n
-  | otherwise = lift $ initializeFromSeed seed >>= ba n m where
+  | otherwise = lift $ initializeFromSeed seed >>= ba n m
+  where
     ba :: Int -> Int -> MonadGen m -> m g
-    ba n m gen = do 
+    ba n m gen = do
       ses <- g m (S.fromList [0 .. m - 1]) S.empty S.empty
       return $ build [0 .. m - 1] nb ses eb
       where
         g :: Int -> Seq Int -> Seq EdgeId -> Seq Int -> m (Seq EdgeId)
         g s ts es rns
           | s == n = return es
-          | otherwise = let
-              ss = S.replicate m s
-              !rns' = rns >< ss >< ts
-            in rc rns' m >>= g (s + 1) rns' (es >< S.zip ss ts)
+          | otherwise =
+            let ss = S.replicate m s
+                !rns' = rns >< ss >< ts
+             in rc rns' m >>= g (s + 1) rns' (es >< S.zip ss ts)
         rc :: Seq Int -> Int -> m (Seq Int)
-        rc seq n = loop St.empty 0 where
-          l = S.length seq
-          loop :: Set Int -> Int -> m (Seq Int)
-          loop t s
-            | s == m = return $ S.fromList $ St.toList t
-            | otherwise = do
+        rc seq n = loop St.empty 0
+          where
+            l = S.length seq
+            loop :: Set Int -> Int -> m (Seq Int)
+            loop t s
+              | s == m = return $ S.fromList $ St.toList t
+              | otherwise = do
                 i <- uniformR (0, l - 1) gen
                 let !x = fromJust $ seq S.!? i
                 loop (St.insert x t) (if member x t then s + 1 else s)
--- src/GT/Graph/Internal.hs
+++ src/GT/Graph/Internal.hs
@@ -1,76 +1,94 @@
-{-# LANGUAGE Strict #-}
-{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE Strict #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 
-
 module GT.Graph.Internal where
 
-import GT.Graph.Class
-
+import Control.Lens (Cons, TraversableWithIndex, index, itraversed, (<|), (^?), (^?!))
+import Control.Lens.Indexed (FoldableWithIndex, iany, ifoldl, ifoldlM, ifoldr, ifor, iforM, iforM_)
+import Control.Monad (foldM, forM, forM_, guard)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Maybe (MaybeT (..))
+import Data.Foldable (foldlM, length)
 import Data.HashMap.Strict as HM (HashMap, insert)
-import Data.HashSet as HS (HashSet, insert, filter)
+import Data.HashSet as HS (HashSet, filter, insert)
 import Data.IntMap.Strict as IM (IntMap, insert)
 import Data.Map.Strict as M (Map, insert)
 import Data.Maybe (fromJust)
+import Data.Proxy (Proxy (..))
 import Data.Sequence (Seq)
-import Data.Set as S (Set, insert, filter)
+import Data.Set as S (Set, filter, insert)
+import Data.Traversable (for, sequenceA)
 import Data.Vector as V (Vector)
-
-import Data.Foldable (length, foldlM)
-import Data.Proxy (Proxy(..))
-import Data.Traversable (sequenceA, for)
-import Data.Witherable as W (FilterableWithIndex, ifilter, Filterable, filter)
-
-import Control.Lens ((^?), (^?!), Cons, (<|), TraversableWithIndex, itraversed, index)
-import Control.Lens.Indexed (FoldableWithIndex, iany, ifoldl, ifoldr, iforM_, ifoldlM, ifor, iforM)
-
-import Control.Monad (guard, forM, forM_, foldM)
-import Control.Monad.Trans.Maybe (MaybeT(..))
-import Control.Monad.Trans.Class (lift)
-
+import Data.Witherable as W (Filterable, FilterableWithIndex, filter, ifilter)
 import GHC.Exts (Constraint)
-
+import GT.Graph.Class
+  ( Builder (..),
+    Directing (..),
+    Direction (..),
+    EWith,
+    EdgeId,
+    Graph (..),
+    NWith,
+    NodeId,
+    Unwrap,
+    Wrap (..),
+    opposite',
+  )
 import Prelude hiding (filter)
 
 data G nc n n' ec e e' (d :: Direction) = G
-  { nid :: !Int -- next node id
-  , nc :: !Int
-  , ec :: !Int
-  , ns :: !(nc n)
-  , es :: !(ec e)
-  , prx :: Proxy d
+  { nid :: !Int, -- next node id
+    nc :: !Int,
+    ec :: !Int,
+    ns :: !(nc n),
+    es :: !(ec e),
+    prx :: Proxy d
   }
 
 type VGr n e = G (M.Map NodeId) n (NWith n) (HM.HashMap EdgeId) e (EWith e)
+
 type DiVGr n e = VGr n e 'Directed
+
 type UndiVGr n e = VGr n e 'Undirected
 
 type NVGr n = G (M.Map NodeId) n (NWith n) S.Set EdgeId EdgeId
+
 type DiNVGr n = NVGr n 'Directed
+
 type UndiNVGr n = NVGr n 'Undirected
 
 type EVGr e = G S.Set NodeId NodeId (HM.HashMap EdgeId) e (EWith e)
+
 type DiEVGr e = EVGr e 'Directed
+
 type UndiEVGr e = EVGr e 'Undirected
 
 type Gr = G S.Set NodeId NodeId S.Set EdgeId EdgeId
+
 type DiGr = Gr 'Directed
+
 type UndiGr = Gr 'Undirected
 
 {- backword compatibiilty -}
 type MapGr n e = VGr n e
+
 type DiMapGr n e = MapGr n e 'Directed
+
 type UndiMapGr n e = MapGr n e 'Undirected
+
 type BasicGr = Gr
+
 type DiBasicGr = BasicGr 'Directed
+
 type UndiBasicGr = BasicGr 'Undirected
 
 instance {-# OVERLAPS #-} (Wrap EdgeId e e', SemiFoldable EdgeId ec e, Show e, Show e') => Show (G nc NodeId n' ec e e' d) where
@@ -79,57 +97,87 @@ instance {-# OVERLAPS #-} (Wrap EdgeId e e', SemiFoldable EdgeId ec e, Show e, S
 instance (Wrap EdgeId e e', SemiIndexable NodeId nc n, SemiFoldable EdgeId ec e, Show n, Show e, Show e') => Show (G nc n n' ec e e' d) where
   show g = show $ sfoldr @EdgeId (\p@(i, j) e a -> (wrap p e :: e', sindex (ns g) i, sindex (ns g) j) : a) [] $ es g
 
-_adjCombinator :: f -> NodeId -> ns -> es -> (NodeId -> ns -> Bool) -> (NodeId -> n')
-  -> (NodeId -> es -> es) -> ((EdgeId -> n') -> f -> es -> a) -> Maybe a
+_adjCombinator ::
+  f ->
+  NodeId ->
+  ns ->
+  es ->
+  (NodeId -> ns -> Bool) ->
+  (NodeId -> n') ->
+  (NodeId -> es -> es) ->
+  ((EdgeId -> n') -> f -> es -> a) ->
+  Maybe a
 _adjCombinator f i ns es p gn esf conv = do
   guard $ p i ns
   return $ conv (gn . opposite' i) f (esf i es)
 
 oppositeV' :: (Wrap NodeId n n', SemiIndexable NodeId nc n) => nc n -> NodeId -> EdgeId -> n'
-oppositeV' ns i p = wrap j (sindex ns j) where
-  j = opposite' i p
+oppositeV' ns i p = wrap j (sindex ns j)
+  where
+    j = opposite' i p
 {-# INLINE oppositeV' #-}
 
 filterWithSource :: (SemiFilterable EdgeId ec e, Directing d) => Proxy d -> ec e -> NodeId -> ec e
 filterWithSource prx es i = sfilter (pisSource prx i) es
 {-# INLINE filterWithSource #-}
 
-_adjFCombinator :: (SemiFoldable NodeId nc n, SemiIndexable NodeId nc n, SemiFilterable EdgeId ec e, Directing d, Wrap EdgeId e e', Wrap NodeId n n')
-  => Proxy d -> nc n -> ec e
-  -> ((a -> EdgeId -> e -> b) -> q -> ec e -> r)
-  -> (a -> n' -> e' -> b) -> q -> NodeId -> Maybe r
+_adjFCombinator ::
+  (SemiFoldable NodeId nc n, SemiIndexable NodeId nc n, SemiFilterable EdgeId ec e, Directing d, Wrap EdgeId e e', Wrap NodeId n n') =>
+  Proxy d ->
+  nc n ->
+  ec e ->
+  ((a -> EdgeId -> e -> b) -> q -> ec e -> r) ->
+  (a -> n' -> e' -> b) ->
+  q ->
+  NodeId ->
+  Maybe r
 _adjFCombinator prx ns es g f r i = do
   guard $ selem i ns
   return $ g (\a p e -> f a (oppositeV' ns i p) (wrap p e)) r (filterWithSource prx es i)
 {-# INLINE _adjFCombinator #-}
 
-_adjIFCombinator :: (SemiFoldable NodeId nc n, SemiIndexable NodeId nc n, SemiFilterable EdgeId ec e, Directing d)
-  => Proxy d -> nc n -> ec e
-  -> ((a -> EdgeId -> e -> b) -> q -> ec e -> r)
-  -> (a -> NodeId -> EdgeId -> b) -> q -> NodeId -> Maybe r
+_adjIFCombinator ::
+  (SemiFoldable NodeId nc n, SemiIndexable NodeId nc n, SemiFilterable EdgeId ec e, Directing d) =>
+  Proxy d ->
+  nc n ->
+  ec e ->
+  ((a -> EdgeId -> e -> b) -> q -> ec e -> r) ->
+  (a -> NodeId -> EdgeId -> b) ->
+  q ->
+  NodeId ->
+  Maybe r
 _adjIFCombinator prx ns es g f r i = do
   guard $ selem i ns
   return $ g (\a p e -> f a (opposite' i p) p) r (filterWithSource prx es i)
 {-# INLINE _adjIFCombinator #-}
 
-_adjMCombinator :: (SemiFoldable NodeId nc n, SemiIndexable NodeId nc n, SemiFilterable EdgeId ec e, Directing d, Wrap EdgeId e e', Wrap NodeId n n')
-  => Proxy d -> nc n -> ec e
-  -> ((EdgeId -> e -> b) -> ec e -> r)
-  -> (n' -> e' -> b) -> NodeId -> Maybe r
+_adjMCombinator ::
+  (SemiFoldable NodeId nc n, SemiIndexable NodeId nc n, SemiFilterable EdgeId ec e, Directing d, Wrap EdgeId e e', Wrap NodeId n n') =>
+  Proxy d ->
+  nc n ->
+  ec e ->
+  ((EdgeId -> e -> b) -> ec e -> r) ->
+  (n' -> e' -> b) ->
+  NodeId ->
+  Maybe r
 _adjMCombinator prx ns es g f i = do
   guard $ selem i ns
   return $ g (\p e -> f (oppositeV' ns i p) (wrap p e)) (filterWithSource prx es i)
 {-# INLINE _adjMCombinator #-}
 
-_adjIMCombinator :: (SemiFoldable NodeId nc n, SemiIndexable NodeId nc n, SemiFilterable EdgeId ec e, Directing d)
-  => Proxy d -> nc n -> ec e
-  -> ((EdgeId -> e -> b) -> ec e -> r)
-  -> (NodeId -> EdgeId -> b) -> NodeId -> Maybe r
+_adjIMCombinator ::
+  (SemiFoldable NodeId nc n, SemiIndexable NodeId nc n, SemiFilterable EdgeId ec e, Directing d) =>
+  Proxy d ->
+  nc n ->
+  ec e ->
+  ((EdgeId -> e -> b) -> ec e -> r) ->
+  (NodeId -> EdgeId -> b) ->
+  NodeId ->
+  Maybe r
 _adjIMCombinator prx ns es g f i = do
   guard $ selem i ns
   return $ g (\p _ -> f (opposite' i p) p) (filterWithSource prx es i)
 {-# INLINE _adjIMCombinator #-}
-
 
 class Foldable f => SemiFoldable i f v where
   selem :: i -> f v -> Bool
@@ -150,7 +198,6 @@ class SemiTraversable i t v where
   sfor :: Applicative f => t v -> (i -> v -> f r) -> f (t r)
   sforM :: Monad m => t v -> (i -> v -> m r) -> m (t r)
 
-
 instance {-# OVERLAPS #-} (Eq a, Foldable f) => SemiFoldable a f a where
   selem = elem
   {-# INLINE selem #-}
@@ -167,7 +214,6 @@ instance {-# OVERLAPS #-} (Eq a, Foldable f) => SemiFoldable a f a where
   iconvert f ts = fst $ foldr (\a (s, idx) -> (f idx a a <| s, idx + 1)) (mempty, 0) ts
   {-# INLINE iconvert #-}
 
-
 instance (Eq i, FoldableWithIndex i (f i)) => SemiFoldable i (f i) a where
   selem i = iany (\i' _ -> i == i')
   {-# INLINE selem #-}
@@ -183,7 +229,6 @@ instance (Eq i, FoldableWithIndex i (f i)) => SemiFoldable i (f i) a where
   {-# INLINE convert #-}
   iconvert f ts = fst $ ifoldr (\i a (s, idx) -> (f idx i a <| s, idx + 1)) (mempty, 0) ts
   {-# INLINE iconvert #-}
-
 
 instance {-# OVERLAPS #-} (Eq a, Filterable f) => SemiFilterable a f a where
   sfilter = W.filter
@@ -206,7 +251,7 @@ instance {-# OVERLAPS #-} SemiIndexable a t a where
   {-# INLINE sindex #-}
 
 instance (Eq i, TraversableWithIndex i (t i)) => SemiIndexable i (t i) a where
-  sindex f i = f ^?!itraversed.index i
+  sindex f i = f ^?! itraversed . index i
   {-# INLINE sindex #-}
 
 instance {-# OVERLAPS #-} (Eq a, Traversable t) => SemiTraversable a t a where
@@ -222,11 +267,13 @@ instance (Eq i, TraversableWithIndex i (t i)) => SemiTraversable i (t i) a where
   {-# INLINE sforM #-}
 
 type SemiWitherable i t v = (SemiFoldable i t v, SemiFilterable i t v, SemiTraversable i t v)
+
 type SemiFFI i t v = (SemiFoldable i t v, SemiFilterable i t v, SemiIndexable i t v)
 
-
-instance (Directing d, Wrap NodeId n n', Unwrap NodeId n', SemiFFI NodeId nc n, Wrap EdgeId e e', Unwrap EdgeId e', SemiFFI EdgeId ec e)
-  => Graph (G nc n n' ec e e' d) n n' e e' d where
+instance
+  (Directing d, Wrap NodeId n n', Unwrap NodeId n', SemiFFI NodeId nc n, Wrap EdgeId e e', Unwrap EdgeId e', SemiFFI EdgeId ec e) =>
+  Graph (G nc n n' ec e e' d) n n' e e' d
+  where
   pair = ppair . prx
   {-# INLINE pair #-}
   isSource = pisSource . prx
@@ -241,7 +288,7 @@ instance (Directing d, Wrap NodeId n n', Unwrap NodeId n', SemiFFI NodeId nc n, 
   edgeCount = ec
   {-# INLINE edgeCount #-}
 
-  removeEdge i j g = g { ec = length es', es = es' }
+  removeEdge i j g = g {ec = length es', es = es'}
     where
       es' = sfilter (\p -> p /= pair g i j) (es g)
   {-# INLINE removeEdge #-}
@@ -286,7 +333,6 @@ instance (Directing d, Wrap NodeId n n', Unwrap NodeId n', SemiFFI NodeId nc n, 
   adjIForM_ i g f = MaybeT $ sequenceA $ _adjIMCombinator (prx g) (ns g) (es g) (flip sforM_) f i
   {-# INLINE adjIForM_ #-}
 
-
 class (Foldable nc, Monoid (nc n)) => NodeBuilderBase nc n where
   _nassoc' :: (Foldable f, Wrap NodeId n n', SemiFoldable EdgeId ec e) => f n' -> ec e -> (Int, Int, nc n)
 
@@ -301,11 +347,11 @@ instance NodeBuilderBase HashSet NodeId where
 instance NodeBuilderBase (Map NodeId) n where
   _nassoc' = _nassoc M.insert (const id)
   {-# INLINE _nassoc' #-}
- 
+
 instance NodeBuilderBase (HashMap NodeId) n where
   _nassoc' = _nassoc HM.insert (const id)
   {-# INLINE _nassoc' #-}
- 
+
 instance NodeBuilderBase [] NodeId where
   _nassoc' = _cnassoc'
   {-# INLINE _nassoc' #-}
@@ -337,74 +383,46 @@ instance Directing d => EdgeBuilderBase d (HashMap EdgeId) e where
   _eassoc' prx = _eassoc @d prx HM.insert
   {-# INLINE _eassoc' #-}
 
-
-_cnassoc' :: (Foldable f, Foldable nc, Monoid (nc NodeId), Wrap NodeId NodeId n', SemiFoldable EdgeId ec e, Cons (nc NodeId) (nc NodeId) NodeId NodeId)
-  => f n' -> ec e -> (Int, Int, nc NodeId)
+_cnassoc' ::
+  (Foldable f, Foldable nc, Monoid (nc NodeId), Wrap NodeId NodeId n', SemiFoldable EdgeId ec e, Cons (nc NodeId) (nc NodeId) NodeId NodeId) =>
+  f n' ->
+  ec e ->
+  (Int, Int, nc NodeId)
 _cnassoc' = _nassoc (\a _ s -> a <| s) (const id)
 {-# INLINE _cnassoc' #-}
 
-_nassoc :: (Foldable f, Foldable nc, Monoid (nc n), Wrap NodeId n n', SemiFoldable EdgeId ec e)
-  => (NodeId -> n -> nc n -> nc n) -> (NodeId -> nc n  -> nc n) -> f n' -> ec e -> (Int, Int, nc n)
-_nassoc f' f'' ns es = (h', length ns'', ns'') where
-  (ns'', h') = sfoldr @EdgeId (\(i, j) _ (s, h) -> (f'' j $ f'' i s, max i $ max j h)) (ns', h) es
-  (ns', h) = wfoldr @NodeId (\i n (s, h) -> (f' i n s, max i h)) (mempty, 0) ns
+_nassoc ::
+  (Foldable f, Foldable nc, Monoid (nc n), Wrap NodeId n n', SemiFoldable EdgeId ec e) =>
+  (NodeId -> n -> nc n -> nc n) ->
+  (NodeId -> nc n -> nc n) ->
+  f n' ->
+  ec e ->
+  (Int, Int, nc n)
+_nassoc f' f'' ns es = (h', length ns'', ns'')
+  where
+    (ns'', h') = sfoldr @EdgeId (\(i, j) _ (s, h) -> (f'' j $ f'' i s, max i $ max j h)) (ns', h) es
+    (ns', h) = wfoldr @NodeId (\i n (s, h) -> (f' i n s, max i h)) (mempty, 0) ns
 {-# INLINE _nassoc #-}
 
-_eassoc :: (Directing d, Foldable f, Monoid (ec e), SemiFoldable EdgeId ec e, Wrap EdgeId e e') => Proxy d -> (EdgeId -> e -> ec e -> ec e) -> f e' -> (Int, ec e)
-_eassoc prx f es = (length es', es') where
-  es' = wfoldr @EdgeId (\(i', j') e s -> if i' == j' then s else f (ppair prx i' j') e s) mempty es
+_eassoc ::
+  (Directing d, Foldable f, Monoid (ec e), SemiFoldable EdgeId ec e, Wrap EdgeId e e') =>
+  Proxy d ->
+  (EdgeId -> e -> ec e -> ec e) ->
+  f e' ->
+  (Int, ec e)
+_eassoc prx f es = (length es', es')
+  where
+    es' = wfoldr @EdgeId (\(i', j') e s -> if i' == j' then s else f (ppair prx i' j') e s) mempty es
 {-# INLINE _eassoc #-}
 
+instance
 
 instance (Directing d, Wrap NodeId n n', Wrap EdgeId e e', NodeBuilderBase nc n, EdgeBuilderBase d ec e, SemiFoldable EdgeId ec e)
   => Builder (G nc n n' ec e e' d) n e where
   assoc ns es = G (h' + 1) nc ec ns'' es' prx where
-    prx = Proxy
-    (ec, es') = _eassoc' @d prx es
-    (h', nc, ns'') = _nassoc' ns es'
+      prx = Proxy
+      (ec, es') = _eassoc' @d prx es
+      (h', nc, ns'') = _nassoc' ns es'
   {-# INLINE assoc #-}
   build ns nf es ef = assoc (fmap (\i -> wrap i (nf i) :: n') ns) (fmap (\p@(i, j) -> wrap p (ef i j) :: e') es)
   {-# INLINE build #-}
-
--- _nbuild :: (Foldable nc, Monoid (nc n), SemiFoldable EdgeId ec e) => (NodeId -> n -> nc n -> nc n) -> (NodeId -> n) -> ec e -> (Int, Int, nc n)
--- _nbuild f' nf es = (h', length ns'', ns'') where
---   (ns'', h') = sfoldr @EdgeId (\(i, j) _ (s, h) -> (f' j (nf j) (f' i (nf i) s), max i $ max j h)) (mempty, 0) es
-
--- _ebuild :: (Pairing d, SemiFoldable NodeId nc n, Monoid (ec e), SemiFoldable EdgeId ec e) => Proxy d -> nc n -> (EdgeId -> e -> ec e -> ec e) -> (Int, ec e)
--- _ebuild prx ns ef = (length es', es') where
---   es' = wfoldr @EdgeId (\(i', j') e s -> if i' == j' then s else f (pairing prx i' j') e s) mempty ns
-
-
-{-
-accum :: a -> ST s (V.MVector s a) -> ST s (V.MVector s a)
-accum a svec = do
-  vec <- svec
-  let p = VM.length vec
-  vec' <- VM.grow vec 1
-  VM.write vec' p a
-  return vec'
-
-intmapToVector :: forall v a . (IM.Key -> v -> a) -> IM.IntMap v -> V.Vector a
-intmapToVector f hm = V.create stv where
-  stv :: forall s . ST s (V.MVector s a)
-  stv = IM.foldlWithKey (\s k v -> accum (f k v) s) (VM.new 0) hm
-
-hashmapToVector :: forall k v a . (k -> v -> a) -> H.HashMap k v -> V.Vector a
-hashmapToVector f hm = V.create stv where
-  stv :: forall s . ST s (V.MVector s a)
-  stv = H.foldlWithKey' (\s k v -> accum (f k v) s) (VM.new 0) hm
-
-toVector :: forall f v a . Foldable f => (v -> a) -> f v -> V.Vector a
-toVector f hs = V.create stv where
-  stv :: forall s . ST s (V.MVector s a)
-  stv = foldl
-    (\sv v -> do
-      vec <- sv
-      let p = VM.length vec
-      vec' <- VM.grow vec 1
-      VM.write vec' p $ f v
-      return vec'
-    )
-    (VM.new 0)
-    hs
--}
